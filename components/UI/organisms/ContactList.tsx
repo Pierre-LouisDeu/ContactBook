fee5525fbbac8131145e90db7069f2218cacc8ad
Pierre-LouisDeu <73912685+Pierre-LouisDeu@users.noreply.github.com>
2022-09-29 16:29:24 +0200

    feat: add loader and error handler

--- components/UI/organisms/ContactList.tsx
+++ components/UI/organisms/ContactList.tsx
@@ -7,32 +7,37 @@ import useFetch from "../../../hooks/useFetch";
 // };
 
 const ContactList: React.FunctionComponent<any> = ({ setOpen }) => {
-  const [contacts] = useFetch('http://localhost:8000/contacts');
+  const [contacts, isPending, error] = useFetch(
+    "http://localhost:8000/contacts"
+  );
 
   return (
     <>
       <div className="mt-2 w-full">
-        {contacts && contacts.map(
-          (contact: {
-            id: number;
-            firstName: string;
-            lastName: string;
-            email: string;
-            birthday: string;
-          }) => (
-            <div className="py-4">
-              <ContactCard
-                key={contact.id}
-                id={contact.id}
-                firstName={contact.firstName}
-                lastName={contact.lastName}
-                email={contact.email}
-                birthday={contact.birthday}
-                setOpen={setOpen}
-              />
-            </div>
-          )
-        )}
+        {error && <div>{error}</div>}
+        {isPending && <div>Loading...</div>}
+        {contacts &&
+          contacts.map(
+            (contact: {
+              id: number;
+              firstName: string;
+              lastName: string;
+              email: string;
+              birthday: string;
+            }) => (
+              <div className="py-4">
+                <ContactCard
+                  key={contact.id}
+                  id={contact.id}
+                  firstName={contact.firstName}
+                  lastName={contact.lastName}
+                  email={contact.email}
+                  birthday={contact.birthday}
+                  setOpen={setOpen}
+                />
+              </div>
+            )
+          )}
       </div>
     </>
   );
