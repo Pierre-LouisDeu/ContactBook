be8164963be0bb5f9c0d1fcbf5fc9daa62ac4be8
Pierre-LouisDeu <73912685+Pierre-LouisDeu@users.noreply.github.com>
2022-09-29 15:54:47 +0200

    feat: add useFetch hook

--- components/UI/organisms/ContactList.tsx
+++ components/UI/organisms/ContactList.tsx
@@ -1,49 +1,18 @@
 import React, { useState, useEffect } from "react";
 import ContactCard from "../molecules/ContactCard";
+import useFetch from "../../../hooks/useFetch";
 
 // type ContactListProps =  {
 //   contacts : Array<{ name: string, email: string, birthday: string}>;
 // };
 
-const contacts = [
-  {
-    id: 1,
-    firstName: "Bob",
-    lastName: "Bob",
-    email: "bob@email.com",
-    birthday: "01/01/2021",
-  },
-  {
-    id: 2,
-    firstName: "John",
-    lastName: "Alan",
-    email: "alan@email.com",
-    birthday: "01/01/2021",
-  },
-  {
-    id: 3,
-    firstName: "Juliette",
-    lastName: "Z",
-    email: "juliette@email.com",
-    birthday: "09/09/2021",
-  },
-];
-
 const ContactList: React.FunctionComponent<any> = ({ setOpen }) => {
-  const [contacts, setContacts] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/contacts").then((res) =>
-      res.json().then((data) => {
-        setContacts(data);
-      })
-    );
-  }, []);
+  const [contacts] = useFetch('http://localhost:8000/contacts');
 
   return (
     <>
       <div className="mt-2 w-full">
-        {contacts.map(
+        {contacts && contacts.map(
           (contact: {
             id: number;
             firstName: string;
